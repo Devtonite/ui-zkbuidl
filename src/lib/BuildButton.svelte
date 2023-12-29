6dffd7460c74a0596cc6abe05076e9d60f5d9ddb
devtonite <devtonite001@gmail.com>
2023-12-28 21:21:35 -0600

    Create button components for builders and hunters

---
+++ src/lib/BuildButton.svelte
@@ -0,0 +1,10 @@
+<script>
+
+    import BuildIcon from "./BuildIcon.svelte";
+
+</script>
+
+<a href="/build" class="flex flex-col items-center">
+    BUILD
+    <BuildIcon/>
+</a>
---
+++ src/lib/HuntButton.svelte
@@ -0,0 +1,14 @@
+<script>
+
+	import FalconIcon from "./FalconIcon.svelte";
+import LionIcon from "./LionIcon.svelte";
+	import TigerIcon from "./TigerIcon.svelte";
+
+</script>
+
+<a href="/hunt" class="flex flex-col items-center">
+    HUNT
+    <!-- <LionIcon/> -->
+    <!-- <TigerIcon/> -->
+    <FalconIcon/>
+</a>
